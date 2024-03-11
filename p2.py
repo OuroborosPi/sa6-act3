3514fb273b457083b8abd09cd23fbd5fdc918e48
Mario Vera <callmejumpking@proton.me>
2024-03-11 11:44:08 -0500

    Fixed the Lambda in order to sort by alphabetical order if it's a tie in word length

--- p2.py
+++ p2.py
@@ -1,3 +1,3 @@
 string_list = ['Hello','Cat','Dog','Elephant','pneumonoultramicroscopicsilicovolcanoconiosis','Bird','Tiger']
-list_sorter = sorted(string_list, key= lambda x: len(x))
+list_sorter = sorted(string_list, key= lambda x: (len(x), x))
 print(f'The list sorted by length: {list_sorter}')
